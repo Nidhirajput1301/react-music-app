d3860f0073168700886f654420734b64e85ac43f
Nidhirajput1301 <nidhirajput1301@gmail.com>
2020-09-17 22:19:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    // BEM
+    <div>
+      <h1>hello guys</h1>
+
+      {/* spotify logo */}
+      {/* login with spotify button */}
+      
+    </div>
+  );
+}
+
+export default App;
