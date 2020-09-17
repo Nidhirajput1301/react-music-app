1667cfc4fc2317980d7d4fcf3766e98f1968e47c
Nidhirajput1301 <nidhirajput1301@gmail.com>
2020-09-18 00:03:16 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  // run code based on a given condition
+  useEffect(() => {
+    // code...
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token);
+    }
+
+    console.log("i have", token);
+  }, []);
   return (
     // BEM
     <div>
-      <h1>hello guys</h1>
-
-      {/* spotify logo */}
-      {/* login with spotify button */}
-      
+      {
+        token ? <h1>logged in</h1> : (<Login />)
+      }
     </div>
   );
 }
